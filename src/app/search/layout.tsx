b07eeaa9f9070d259d495008171f54355a8ae8a3
Saleem Maini <saleem.maini@miniandmore.co>
2023-09-01 16:36:42 +0300

    implement the layout

---
+++ src/app/search/layout.tsx
@@ -0,0 +1,11 @@
+import { SearchPageLayoutView } from '@/views/search/layout'
+
+const SearchPageLayout = () => {
+  return (
+    <div>
+      <SearchPageLayoutView />
+    </div>
+  )
+}
+
+export default SearchPageLayout
---
+++ src/views/search/search-filters/index.tsx
@@ -0,0 +1,7 @@
+export const SearchFilters = () => {
+  return (
+    <div className='bg-purple-500 p-10'>
+      <p> SearchFilters</p>
+    </div>
+  )
+}

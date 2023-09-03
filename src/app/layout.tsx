fabb4e6ef992cfe0edb6942e83cd81904da6518d
Saleem Maini <saleem.maini@miniandmore.co>
2023-09-03 23:32:22 +0300

    layouts and routes enhancements

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
-import { Navbar } from '@/layout/navbar'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import { Footer } from '@/layout/footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,11 +12,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en' data-theme='light'>
-      <body className={inter.className}>
-        <Navbar />
-        {children}
-        <Footer />
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   )
 }

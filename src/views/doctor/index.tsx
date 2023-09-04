32285b6eceaf7a7c7be1f3ba3c251613c5c0db6f
Saleem Maini <saleem.maini@miniandmore.co>
2023-09-04 09:35:09 +0300

    initialize appointment card

--- src/views/doctor/index.tsx
+++ src/views/doctor/index.tsx
@@ -1,5 +1,6 @@
 import { Doctor } from '@/types/doctors'
 import { DoctorDetailsCard } from './doctor-details-card'
+import { PickAppointmentCard } from './pick-appointment-card'
 
 type Props = {
   doctorData: Doctor
@@ -9,8 +10,9 @@ export const DoctorPageView = (props: Props) => {
   const { doctorData } = props
 
   return (
-    <div className='w-full'>
+    <div className='w-full flex flex-col gap-5'>
       <DoctorDetailsCard data={doctorData} />
+      <PickAppointmentCard />
     </div>
   )
 }
---
+++ src/views/doctor/pick-appointment-card/index.tsx
@@ -0,0 +1,7 @@
+export const PickAppointmentCard = () => {
+  return (
+    <div className='card w-full bg-base-100 shadow-xl min-h-fit items-center p-5'>
+      <p className='card-title text-2xl'>Book Appointment</p>
+    </div>
+  )
+}

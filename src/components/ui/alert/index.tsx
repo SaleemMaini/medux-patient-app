32e9d20607cf8b4b61db0b1068a6a8fb9e7d2c32
Saleem Maini <saleem.maini@miniandmore.co>
2023-09-08 22:02:53 +0300

    enhancements

--- src/components/ui/alert/index.tsx
+++ src/components/ui/alert/index.tsx
@@ -44,7 +44,7 @@ export const Alert = (props: Props) => {
             d='M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z'
           />
         </svg>
-        <span>{text}</span>
+        <span className='font-semibold'>{text}</span>
       </div>
     )
   }
